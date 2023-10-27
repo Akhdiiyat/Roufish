0e8328ff405c9d4d78f33099c83e71371ae6da09
jyasyeer <muhammadjasirnabil@gmail.com>
2023-10-27 23:15:23 +0700

    tambah fitur
    tambahan :
    loadscreen
    tombol back di register
    
    note :
    ada bug di login ketika hide password(blm fix)
    hide password di register blm dibuat

--- app/src/main/java/com/example/roufish/register.java
+++ app/src/main/java/com/example/roufish/register.java
@@ -1,13 +1,28 @@
 package com.example.roufish;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class register extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
+
+        FloatingActionButton backToMain = findViewById(R.id.backToMainREG);
+
+        backToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(register.this, MainActivity.class);
+
+                startActivity(registerIntent);
+            }
+        });
 
         // Tambahkan kode lain yang diperlukan untuk halaman pendaftaran di sini
     }
