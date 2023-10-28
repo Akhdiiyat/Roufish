2b0b651a059820312e0e1de4430d89dd356ceea6
jyasyeer <muhammadjasirnabil@gmail.com>
2023-10-28 18:20:15 +0700

    tambah fitur pada register
    tambahan :
    - show & hide password
    - koneksi tombol daftar ke halaman login
    
    note :
    bug aman

--- app/src/main/java/com/example/roufish/register.java
+++ app/src/main/java/com/example/roufish/register.java
@@ -3,7 +3,12 @@ package com.example.roufish;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -12,8 +17,20 @@ public class register extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
+        EditText inputPassword = findViewById(R.id.input_Password);
 
         FloatingActionButton backToMain = findViewById(R.id.backToMainREG);
+
+        Button daftar = findViewById(R.id.btn_daftar);
+
+        daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(register.this, login.class);
+
+                startActivity(registerIntent);
+            }
+        });
 
         backToMain.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +41,32 @@ public class register extends Activity {
             }
         });
 
+        inputPassword.setOnTouchListener(new View.OnTouchListener() {
+            boolean passwordView = false;
+            @Override
+            public boolean onTouch(View v, MotionEvent motionEvent) {
+                final int Right = 2;
+
+                if(motionEvent.getAction()== motionEvent.ACTION_UP){
+                    if (motionEvent.getRawX()>=inputPassword.getRight()-inputPassword.getCompoundDrawables()[Right].getBounds().width()){
+                        int selection=inputPassword.getSelectionEnd();
+                        if(passwordView){
+                            inputPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.visibilityoff_icon,0);
+                            inputPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                            passwordView=false;
+                        }else{
+                            inputPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.visibility_icon,0);
+                            inputPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                            passwordView=true;
+                        }
+                        inputPassword.setSelection(selection);
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        });
         // Tambahkan kode lain yang diperlukan untuk halaman pendaftaran di sini
     }
 }
