2a36be208b3e8cc1024ff8a1ffd2401183c1bc64
jyasyeer <muhammadjasirnabil@gmail.com>
2023-11-03 01:25:54 +0700

    udh konek sampe halaman pelelangan

--- app/src/main/java/com/example/roufish/halamanProduct.java
+++ app/src/main/java/com/example/roufish/halamanProduct.java
@@ -2,7 +2,10 @@ package com.example.roufish;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class halamanProduct extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class halamanProduct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_product);
+
+        RadioButton lelang = findViewById(R.id.lelang);
+
+        lelang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent lelang = new Intent(halamanProduct.this, pelelangan.class);
+
+                startActivity(lelang);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/roufish/pelelangan.java
@@ -0,0 +1,14 @@
+package com.example.roufish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pelelangan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pelelangan);
+    }
+}
