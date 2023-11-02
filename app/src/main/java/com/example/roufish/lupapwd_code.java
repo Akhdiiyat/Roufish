e8b54525b486a53f5757db7e295f7e9ca227e812
Reva <rivandi37@gmail.com>
2023-11-03 00:08:54 +0700

    lupapwd_verif belum input text sama tombol verifikasi

--- app/src/main/java/com/example/roufish/lupapwd_code.java
+++ app/src/main/java/com/example/roufish/lupapwd_code.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class lupapwd_code extends AppCompatActivity {
@@ -12,13 +14,21 @@ public class lupapwd_code extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lupapwd_code);
-
+        Button buttonVerif = findViewById(R.id.btn_verifikasi);
         FloatingActionButton backToMain = findViewById(R.id.backToMainREG); // Ensure the ID is correct
         backToMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent lupaPwIntent = new Intent(lupapwd_code.this, lupapwd.class);
                 startActivity(lupaPwIntent);
+            }
+        });
+
+        buttonVerif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent verifIntent = new Intent(lupapwd_code.this, lupapwd_verif.class);
+                startActivity(verifIntent);
             }
         });
     }
--- app/src/main/java/com/example/roufish/lupapwd_verif.java
+++ app/src/main/java/com/example/roufish/lupapwd_verif.java
@@ -2,13 +2,26 @@ package com.example.roufish;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class lupapwd_verif extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        FloatingActionButton backLupaCode = findViewById(R.id.backToMainREG);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lupapwd_verif);
+        backLupaCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent codeIntent = new Intent(lupapwd_verif.this, lupapwd_code.class);
+                startActivity(codeIntent);
+            }
+        });
     }
 }
