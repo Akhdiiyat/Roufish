bcce068bc352cf3611a1e47e08f5f2b295cca35c
jyasyeer <muhammadjasirnabil@gmail.com>
2023-11-08 23:01:09 +0700

    fix bug lupa password
    bismillah ga ada bug lagi

--- app/src/main/java/com/example/roufish/lupapwd.java
+++ app/src/main/java/com/example/roufish/lupapwd.java
@@ -6,38 +6,41 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class lupapwd extends AppCompatActivity {
 
     Button buttonSend;
-    TextInputLayout emailInputLayout = findViewById(R.id.emailInputLayout);;
-    TextInputEditText emailTextInput = findViewById(R.id.emailTextInput);;
+    EditText emailInput ;;
+    //TextInputEditText emailTextInput = findViewById(R.id.emailTextInput);;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lupapwd);
-        emailInputLayout = findViewById(R.id.emailInputLayout);
-        emailTextInput = findViewById(R.id.emailTextInput);
+        emailInput = findViewById(R.id.emailInput);
+        //emailTextInput = findViewById(R.id.emailTextInput);
         buttonSend = findViewById(R.id.btn_send_code);
 
         buttonSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = emailTextInput.getText().toString().trim();
+                String email ;
+                email = String.valueOf(emailInput.getText());
                 if (TextUtils.isEmpty(email)) {
                     // Email field is empty, show an error message
-                    emailInputLayout.setError("Email is required");
-                } else {
-                    // Email field is not empty, remove any previous error message
-                    emailInputLayout.setError(null);
+                    Toast.makeText(lupapwd.this,"Masukkan username",Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
                     // Proceed with sending the code
                     Intent sendIntent = new Intent(lupapwd.this, lupapwd_code.class);
                     startActivity(sendIntent);
-                }
+
             }
         });
     }
