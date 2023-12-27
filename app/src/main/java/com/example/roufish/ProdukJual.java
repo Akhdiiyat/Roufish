734a1e4cf3b9711577025d841d1edf0c77960387
Rizz <rizkyhadiyanto0505@gmail.com>
2023-12-28 01:02:34 +0700

    tambah produk_jual

---
+++ app/src/main/java/com/example/roufish/ProdukJual.java
@@ -0,0 +1,14 @@
+package com.example.roufish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProdukJual extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_produk_jual);
+    }
+}
